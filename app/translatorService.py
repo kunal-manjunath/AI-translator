31000a7e405decc18c7bede7eaa776ee16e326c1
Kunal Manjunath <40420820+kunal-manjunath@users.noreply.github.com>
2025-04-13 19:55:20 +0530

    remove api key

--- app/translatorService.py
+++ app/translatorService.py
@@ -5,8 +5,7 @@ messages=[
     {"role": "system", "content": "You are a language translator,the user will provide you with the language to translate to and the message to translate"},
 ]
 
-client = OpenAI(api_key="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")
-
+client = OpenAI(api_key="")
 def translate(message: str, language: str = "Spanish"):
     messages.append(
         {
